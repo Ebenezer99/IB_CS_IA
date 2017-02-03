c5d3d6f9e7feaa8e2a04f5c946cb89aa635f8c3b
Ebenezer99 <ebbya12@gmail.com>
2017-02-02 22:23:10 -0500

    Initial commit

---
+++ app/src/main/java/com/example/administrator/ib_cs_ia/Flight_Data.java
@@ -0,0 +1,13 @@
+package com.example.administrator.ib_cs_ia;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Flight_Data extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_flight__data);
+    }
+}
