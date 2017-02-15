9194d22fd0a4c4cd84a69916d3f9731da8ba6870
Ebenezer99 <ebbya12@gmail.com>
2017-02-15 09:58:50 -0500

    2nd

--- app/src/main/java/com/example/administrator/ib_cs_ia/CompareData.java
+++ app/src/main/java/com/example/administrator/ib_cs_ia/CompareData.java
@@ -8,7 +8,7 @@ import android.widget.TextView;
 import java.io.IOException;
 import java.util.*;
 
-import javax.swing.text.View;
+//import javax.swing.text.*;
 
 import com.google.api.client.auth.oauth2.Credential;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
@@ -29,9 +29,25 @@ import com.google.api.services.qpxExpress.model.TripOptionsRequest;
 import com.google.api.services.qpxExpress.model.TripsSearchRequest;
 import com.google.api.services.qpxExpress.model.SliceInput;
 import com.google.api.services.qpxExpress.model.TripsSearchResponse;
+
 public class CompareData extends AppCompatActivity {
 
-    @Override
+    private static final String APPLICATION_NAME = "MyFlightApplication";
+
+    private static final String API_KEY = "AIzaSyDnBCdsmTnrL5XFrO2TjJyvFioswjakNYU";
+
+    /**
+     * Global instance of the HTTP transport.
+     */
+    private static HttpTransport httpTransport;
+
+    /**
+     * Global instance of the JSON factory.
+     */
+    private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
+    private static String org;
+   private static String dest;
+    private static String date;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_compare_data);
@@ -39,131 +55,116 @@ public class CompareData extends AppCompatActivity {
 
         Intent intent = getIntent();
         String message = intent.getStringExtra(Flight_Data.FINAL_DATA);
-        TextView textView = new TextView(this);
-        textView.setTextSize(40);
-        textView.setText(message);
-
-        ViewGroup layout = (ViewGroup) findViewById(R.id.activity_compare_data);
-        layout.addView(textView);
-    }
+        String data[] = message.split(" ");
+        org = data[0];
+        dest = data[1];
+         date = data[2];
 
 
+        //TextView textView = new TextView(this);
+        //textView.setTextSize(40);
+        //textView.setText(org);
 
-
-    public class AirlineReservation {
-
-        /**
-         * @param args
-         */
-
-
-        private static final String APPLICATION_NAME = "MyFlightApplication";
-
-        private static final String API_KEY = "AIzaSyDnBCdsmTnrL5XFrO2TjJyvFioswjakNYU";
-
-        /** Global instance of the HTTP transport. */
-        private static HttpTransport httpTransport;
-
-        /** Global instance of the JSON factory. */
-        private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
+       // ViewGroup layout = (ViewGroup) findViewById(R.id.activity_compare_data);
+        //layout.addView(textView);
 
 
 
 
-        public static void main(String[] args) {
-            // TODO Auto-generated method stub
+        // TODO Auto-generated method stub
 
-            try {
-                httpTransport = GoogleNetHttpTransport.newTrustedTransport();
+        try {
+            httpTransport = GoogleNetHttpTransport.newTrustedTransport();
 
 
-                PassengerCounts passengers= new PassengerCounts();
-                passengers.setAdultCount(1);
+            PassengerCounts passengers = new PassengerCounts();
+            passengers.setAdultCount(1);
 
-                List<SliceInput> slices = new ArrayList<SliceInput>();
+            List<SliceInput> slices = new ArrayList<SliceInput>();
 
-                SliceInput slice = new SliceInput();
-                slice.setOrigin("NYC");
-                slice.setDestination("LGA");
-                slice.setDate("2015-04-29");
-                slices.add(slice);
+            SliceInput slice = new SliceInput();
+            slice.setOrigin(org);
+            slice.setDestination(dest);
+            slice.setDate(date);
+            slices.add(slice);
 
-                TripOptionsRequest request= new TripOptionsRequest();
-                request.setSolutions(10);
-                request.setPassengers(passengers);
-                request.setSlice(slices);
+            TripOptionsRequest request = new TripOptionsRequest();
+            request.setSolutions(10);
+            request.setPassengers(passengers);
+            request.setSlice(slices);
 
-                TripsSearchRequest parameters = new TripsSearchRequest();
-                parameters.setRequest(request);
-                QPXExpress qpXExpress= new QPXExpress.Builder(httpTransport, JSON_FACTORY, null).setApplicationName(APPLICATION_NAME)
-                        .setGoogleClientRequestInitializer(new QPXExpressRequestInitializer(API_KEY)).build();
+            TripsSearchRequest parameters = new TripsSearchRequest();
+            parameters.setRequest(request);
+            QPXExpress qpXExpress = new QPXExpress.Builder(httpTransport, JSON_FACTORY, null).setApplicationName(APPLICATION_NAME)
+                    .setGoogleClientRequestInitializer(new QPXExpressRequestInitializer(API_KEY)).build();
 
-                TripsSearchResponse list= qpXExpress.trips().search(parameters).execute();
-                List<TripOption> tripResults=list.getTrips().getTripOption();
+            TripsSearchResponse list = qpXExpress.trips().search(parameters).execute();
+            List<TripOption> tripResults = list.getTrips().getTripOption();
 
-                String id;
+            String id;
 
-                for(int i=0; i<tripResults.size(); i++){
-                    //Trip Option ID
-                    id= tripResults.get(i).getId();
-                    System.out.println("id "+id);
+            for (int i = 0; i < tripResults.size(); i++) {
+                //Trip Option ID
+                id = tripResults.get(i).getId();
+                System.out.println("id " + id);
 
-                    //Slice
-                    List<SliceInfo> sliceInfo= tripResults.get(i).getSlice();
-                    for(int j=0; j<sliceInfo.size(); j++){
-                        int duration= sliceInfo.get(j).getDuration();
-                        System.out.println("duration "+duration);
-                        List<SegmentInfo> segInfo= sliceInfo.get(j).getSegment();
-                        for(int k=0; k<segInfo.size(); k++){
-                            String bookingCode= segInfo.get(k).getBookingCode();
-                            System.out.println("bookingCode "+bookingCode);
-                            FlightInfo flightInfo=segInfo.get(k).getFlight();
-                            String flightNum= flightInfo.getNumber();
-                            System.out.println("flightNum "+flightNum);
-                            String flightCarrier= flightInfo.getCarrier();
-                            System.out.println("flightCarrier "+flightCarrier);
-                            List<LegInfo> leg=segInfo.get(k).getLeg();
-                            for(int l=0; l<leg.size(); l++){
-                                String aircraft= leg.get(l).getAircraft();
-                                System.out.println("aircraft "+aircraft);
-                                String arrivalTime= leg.get(l).getArrivalTime();
-                                System.out.println("arrivalTime "+arrivalTime);
-                                String departTime=leg.get(l).getDepartureTime();
-                                System.out.println("departTime "+departTime);
-                                String dest=leg.get(l).getDestination();
-                                System.out.println("Destination "+dest);
-                                String destTer= leg.get(l).getDestinationTerminal();
-                                System.out.println("DestTer "+destTer);
-                                String origin=leg.get(l).getOrigin();
-                                System.out.println("origun "+origin);
-                                String originTer=leg.get(l).getOriginTerminal();
-                                System.out.println("OriginTer "+originTer);
-                                int durationLeg= leg.get(l).getDuration();
-                                System.out.println("durationleg "+durationLeg);
-                                int mil= leg.get(l).getMileage();
-                                System.out.println("Milleage "+mil);
-
-                            }
+                //Slice
+                List<SliceInfo> sliceInfo = tripResults.get(i).getSlice();
+                for (int j = 0; j < sliceInfo.size(); j++) {
+                    int duration = sliceInfo.get(j).getDuration();
+                    System.out.println("duration " + duration);
+                    List<SegmentInfo> segInfo = sliceInfo.get(j).getSegment();
+                    for (int k = 0; k < segInfo.size(); k++) {
+                        String bookingCode = segInfo.get(k).getBookingCode();
+                        System.out.println("bookingCode " + bookingCode);
+                        FlightInfo flightInfo = segInfo.get(k).getFlight();
+                        String flightNum = flightInfo.getNumber();
+                        System.out.println("flightNum " + flightNum);
+                        String flightCarrier = flightInfo.getCarrier();
+                        System.out.println("flightCarrier " + flightCarrier);
+                        List<LegInfo> leg = segInfo.get(k).getLeg();
+                        for (int l = 0; l < leg.size(); l++) {
+                            String aircraft = leg.get(l).getAircraft();
+                            System.out.println("aircraft " + aircraft);
+                            String arrivalTime = leg.get(l).getArrivalTime();
+                            System.out.println("arrivalTime " + arrivalTime);
+                            String departTime = leg.get(l).getDepartureTime();
+                            System.out.println("departTime " + departTime);
+                            String dest = leg.get(l).getDestination();
+                            System.out.println("Destination " + dest);
+                            String destTer = leg.get(l).getDestinationTerminal();
+                            System.out.println("DestTer " + destTer);
+                            String origin = leg.get(l).getOrigin();
+                            System.out.println("origun " + origin);
+                            String originTer = leg.get(l).getOriginTerminal();
+                            System.out.println("OriginTer " + originTer);
+                            int durationLeg = leg.get(l).getDuration();
+                            System.out.println("durationleg " + durationLeg);
+                            int mil = leg.get(l).getMileage();
+                            System.out.println("Milleage " + mil);
 
                         }
-                    }
 
-                    //Pricing
-                    List<PricingInfo> priceInfo= tripResults.get(i).getPricing();
-                    for(int p=0; p<priceInfo.size(); p++){
-                        String price= priceInfo.get(p).getSaleTotal();
-                        System.out.println("Price "+price);
                     }
-
                 }
-                return;
-            } catch (IOException e) {
-                System.err.println(e.getMessage());
-            } catch (Throwable t) {
-                t.printStackTrace();
-            }
-            System.exit(1);
 
+                //Pricing
+                List<PricingInfo> priceInfo = tripResults.get(i).getPricing();
+                for (int p = 0; p < priceInfo.size(); p++) {
+                    String price = priceInfo.get(p).getSaleTotal();
+                    System.out.println("Price " + price);
+                }
+
+            }
+            return;
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        } catch (Throwable t) {
+            t.printStackTrace();
         }
+        System.exit(1);
+
+
     }
+
 }
